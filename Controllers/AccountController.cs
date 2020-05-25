8e969849b8b1c8a2a1487c11de7ae75cd0fc5186
Dhruv Shah <dhruvshah2000@hotmail.com>
2020-05-25 13:04:12 -0500

    Added

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -152,6 +152,7 @@ namespace TaskManagementSystem.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -160,7 +161,7 @@ namespace TaskManagementSystem.Controllers
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // var callbackUrl = Url.ActC:\Users\dhruv\source\repos\QandAWebsite\Controllers\RolesController.csion("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Home");
