884e8f52b7465bb3de3a83ac42ecf303bd76f5fa
Rajabali Jawadali <rjava721@gmail.com>
2020-06-04 17:53:28 -0500

    format code and implement Notification in System Design

--- Models/CompletedTaskModel.cs
+++ Models/CompletedTaskModel.cs
@@ -5,14 +5,14 @@ using System.Web;
 
 namespace TaskManagementSystem.Models
 {
-    public class CompletedTaskModel
-    {
-        public int Id { get; set; }
-        public int TaskId { get; set; }
-        public string TaskName { get; set; }
-        public string TaskDesc { get; set; }
-        public DateTime? SubmissionDate { get; set; }
-        public string DeveloperName { get; set; }
-        public string ProjectName { get; set; }
-    }
+	public class CompletedTaskModel
+	{
+		public int Id { get; set; }
+		public int TaskId { get; set; }
+		public string TaskName { get; set; }
+		public string TaskDesc { get; set; }
+		public DateTime? SubmissionDate { get; set; }
+		public string DeveloperName { get; set; }
+		public string ProjectName { get; set; }
+	}
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -7,29 +7,29 @@ using System.Web;
 
 namespace TaskManagementSystem.Models
 {
-   public enum Priority
-    {
-        Urgent,
-        High,
-        Medium,
-        Low,
-    }
-    public class Project
-    {
-        public Project()
-        {
-            this.Tasks = new HashSet<Tasks>();
-            this.ProjectUsers = new HashSet<ProjectUser>();
-        }
-        public Priority? Priority { get; set; }
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [DisplayName("DeadLine")]
-        [DataType(DataType.Date)]
-        public DateTime? Deadline { get; set; }
-        public string Customer { get; set; }
-        public ICollection<Tasks> Tasks { get; set; }
-        public ICollection<ProjectUser> ProjectUsers { get; set; }
-    }
+	public enum Priority
+	{
+		Urgent,
+		High,
+		Medium,
+		Low,
+	}
+	public class Project
+	{
+		public Project()
+		{
+			this.Tasks = new HashSet<Tasks>();
+			this.ProjectUsers = new HashSet<ProjectUser>();
+		}
+		public Priority? Priority { get; set; }
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[DisplayName("DeadLine")]
+		[DataType(DataType.Date)]
+		public DateTime? Deadline { get; set; }
+		public string Customer { get; set; }
+		public ICollection<Tasks> Tasks { get; set; }
+		public ICollection<ProjectUser> ProjectUsers { get; set; }
+	}
 }
